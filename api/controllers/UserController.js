39ddc19a7dd30cda590f56999deaa62e3583a6ae
thodoris semertzidis <tsemertzidis@gmail.com>
2013-11-15 13:52:03 +0200

    a sails app for social-second-screen

---
+++ api/controllers/UserController.js
@@ -0,0 +1,16 @@
+/**
+ * UserController
+ *
+ * @module		:: Controller
+ * @description	:: Contains logic for handling requests.
+ */
+
+module.exports = {
+
+  
+  new: function (req, res) {
+    res.view();
+  }
+  
+
+};
